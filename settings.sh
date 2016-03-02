a97c48d62e9cdaeeff37facd2ec1ff747d2ba313
oubeichen <oubeichen@gmail.com>
2016-03-02 11:37:52 +0800

    bring

--- settings.sh
+++ settings.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SUPPORTED_ARCHITECTURES=(armeabi-v7a armeabi-v7a-neon x86)
+SUPPORTED_ARCHITECTURES=(armeabi-v7a x86)
+ANDROID_NDK=~/android-ndk-r10e/
 ANDROID_NDK_ROOT_PATH=${ANDROID_NDK}
 if [[ -z "$ANDROID_NDK_ROOT_PATH" ]]; then
   echo "You need to set ANDROID_NDK environment variable, please check instructions"
@@ -9,9 +10,10 @@ fi
 ANDROID_API_VERSION=9
 NDK_TOOLCHAIN_ABI_VERSION=4.8
 
-NUMBER_OF_CORES=$(nproc)
+# TODO detect number of cpu cores
+NUMBER_OF_CORES=8
 HOST_UNAME=$(uname -m)
-TARGET_OS=linux
+TARGET_OS=darwin
 
 CFLAGS='-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -fno-strict-overflow -fstack-protector-all'
 LDFLAGS='-Wl,-z,relro -Wl,-z,now -pie'
